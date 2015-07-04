e48c717833a64f290c6af712a3e9f6be9d97fedd
Alejandro Medina <5almero5@gmail.com>
2015-07-04 12:19:01 -0500

    [add] archivo de configuracion, integracion para el servidor

--- index.js
+++ index.js
@@ -4,12 +4,16 @@
  //Servidor 
 import express from 'express';
 import http from 'http'; // este ya viene cargado junto con Node asi que no hace falta instalarlo
+import bodyParser from 'body-parser';
+import r from 'rethinkdb';
+import config from './config'; 
 
-const port = 4000;
+const port = config.express.port;
 const app = express();
 
 //Configuracion de la ruta de archivos estaticos 
 app.use('/',express.static(__dirname + '/public'));
+
 // respode con un envio de archivo cuando un GET request es hecho desde la homepage
 app.get('/',(req,res) => {
 	res.sendFile(__dirname + '/index.html');
